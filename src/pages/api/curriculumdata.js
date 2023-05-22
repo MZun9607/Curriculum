d7cac8389b7aafa7234df805177c2f2eef023482
Mauricio <mzb9607@gmail.com>
2023-05-22 09:56:24 -0500

    Netlify

--- src/pages/api/curriculumdata.js
+++ src/pages/api/curriculumdata.js
@@ -3,17 +3,17 @@ import clientPromise from "../../../lib/mongodb";
 export default async (req, res) => {
     try {
         const client = await clientPromise;
-        const db = client.db("Curriculum");
+        const db = client.db(process.env.MONGODB_DATABASE);
 
        const data = await db
-           .collection("CurriculumData")
+           .collection(process.env.MONGODB_COLLECTION)
            .find({})
            .toArray();
 
        res.json(data);
     } catch (e) {
         console.error(e);
-        res.status(200).json({});
+        res.status(500).json({});
     }
  };
  
